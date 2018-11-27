9b0a5f96153cb47d98e1da05e629e2662f65af22
Questionman <diti94@naver.com>
2018-11-27 09:02:32 +0000

    Initial commit

---
+++ src/B1EventAction.cc
@@ -0,0 +1,101 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1EventAction.cc 93886 2015-11-03 08:28:26Z gcosmo $
+//
+/// \file B1EventAction.cc
+/// \brief Implementation of the B1EventAction class
+
+#include "B1EventAction.hh"
+#include "B1RunAction.hh"
+#include "B1RunData.hh"
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+#include "G4UnitsTable.hh"
+
+#include "Randomize.hh"
+#include <iomanip>
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void B1EventAction::PrintEventStatistics(
+                              G4double absoEdep) const
+{
+  // print event statistics
+  G4cout
+     << "   Absorber: total energy: " 
+     << std::setw(7) << G4BestUnit(absoEdep, "Energy") 
+     << G4endl;
+}
+
+
+B1EventAction::B1EventAction(B1RunAction* runAction)
+: G4UserEventAction(),
+  fRunAction(runAction),
+  fEdep(0.)
+{} 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::~B1EventAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::BeginOfEventAction(const G4Event*)
+{    
+  fEdep = 0.;
+  auto runData 
+    = static_cast<B1RunData*>(
+        G4RunManager::GetRunManager()->GetNonConstCurrentRun());
+  runData->Reset();  
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::EndOfEventAction(const G4Event*)
+{   
+  // accumulate statistics in run action
+  fRunAction->AddEdep(fEdep);
+  if (fEdep > 0)
+  {
+    auto runData = static_cast<B1RunData*>(
+        G4RunManager::GetRunManager()->GetNonConstCurrentRun());
+    runData->FillPerEvent();
+  }
+  //print per event (modulo n)
+  //
+/* auto eventID = event->GetEventID();
+  auto printModulo = G4RunManager::GetRunManager()->GetPrintProgress();
+  if ( ( printModulo > 0 ) && ( eventID % printModulo == 0 ) ) {
+    G4cout << "---> End of event: " << eventID << G4endl;     
+
+//    PrintEventStatistics(runData->GetEdep(kAbs));
+  } */
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
